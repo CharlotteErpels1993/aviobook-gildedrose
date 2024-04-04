42ada66bb466dfd3a586341888ac55c8fccab745
Charlotte Erpels <charlotteerpels@Charlottes-MacBook-Pro.local>
2024-04-04 15:54:37 +0200

    Adds tests for lowering sellIn and quality properties of Items when using updateQuality

--- Tests/GildedRoseTests/GildedRoseTests.swift
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -6,6 +6,30 @@ class GildedRoseTests: XCTestCase {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+        XCTAssertEqual(app.items[0].name, "foo")
+    }
+    
+    func testUpdateQuality_WhenUpdatingQuality_ThenSellInOfItemsIsLowered() {
+        let items = [
+            Item(name: "Item 1", sellIn: 7, quality: 5),
+            Item(name: "Item 2", sellIn: 4, quality: 7)
+        ]
+        
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        
+        XCTAssertEqual(items.map({ $0.sellIn }), [6, 3])
+    }
+    
+    func testUpdateQuality_WhenUpdatingQuality_ThenQualityOfItemsIsLowered() {
+        let items = [
+            Item(name: "Item 1", sellIn: 7, quality: 5),
+            Item(name: "Item 2", sellIn: 4, quality: 7)
+        ]
+        
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        
+        XCTAssertEqual(items.map({ $0.quality }), [4, 6])
     }
 }
